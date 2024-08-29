70f721e7b3033c157ff148969427dc5552b304bc
sneha638 <babysnahe2021@gmail.com>
2024-08-29 14:00:59 +0530

    Project2 Quiz Application

--- Project2/src/main/java/org/example/Main.java
+++ Project2/src/main/java/org/example/Main.java
@@ -13,6 +13,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Quiz quiz = new Quiz();
+
+
         int choice;
         do {
             System.out.println("Quiz Application \n 1. Add Question \n 2. Start Quiz \n 3. View Results \n 4. View all contestants \n 5. Exit \n Enter your choice: ");
